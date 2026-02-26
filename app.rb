544208b28ef65f8eb113a31e86ef9c6f79cb81de
Maxentaxen <max.niemi@elev.ga.ntig.se>
2026-02-26 19:48:05 +0100

    lite saker utöver lektionen, fixade småsaker typ

--- app.rb
+++ app.rb
@@ -62,12 +62,12 @@ class App < Sinatra::Base
 
   get '/index' do 
     p session[:user_id]
-    @movie_ids = db.execute('SELECT movie_id FROM user_watched WHERE user_id = ?', session[:user_id]).first
-    
+    @movie_ids = db.execute('SELECT movie_id FROM user_watched WHERE user_id = ?', session[:user_id]).map(&:values).flatten
+
     if !@movie_ids.nil?
       placeholders = (['?'] * @movie_ids.length).join(',')
       sql = "SELECT name, poster, id FROM movies WHERE id IN (#{placeholders})"
-      @movies = db.execute(sql, @movie_ids.values)
+      @movies = db.execute(sql, @movie_ids)
       ap @movies
       @users = db.execute('SELECT username FROM users where id != ?', session[:user_id])
       erb(:'index')
@@ -94,28 +94,27 @@ class App < Sinatra::Base
 
   post '/add' do
     ap params
-    movienames = db.execute('SELECT name FROM movies').first.values
+    movienames = db.execute('SELECT name FROM movies').map(&:values).flatten
     ap movienames
     movieParams = [params['name'], params['year'].to_i, params['imdb_rating'], params['runtime'], params['poster']]
-    if !movienames.include?(params['name'])
+    if !movienames.include?(params['name']) 
       db.execute('INSERT INTO movies (name, year, imdb_rating, runtime, poster) 
           VALUES (?, ?, ?, ?, ?)', movieParams)
       id = db.execute("SELECT id FROM movies where name=?", params['name']).first.values
   
       params['genre'].each do | genreName |
         id_database_params = [id, genreName.to_i]
-        p id_database_params
         db.execute("INSERT INTO movies_genres (movie_id, genre_id) VALUES (?,?)", id_database_params)
       end
     end
-    userInfo = db.execute('SELECT movie_id FROM user_watched WHERE user_id = ?', session[:user_id]).first
-
+    userInfo = db.execute('SELECT movie_id FROM user_watched WHERE user_id = ?', session[:user_id]).map(&:values).flatten
+    
+    
     if userInfo
-      if !userInfo.values.include?(id)
+      if !userInfo.include?(id)
         db.execute('INSERT INTO user_watched (user_id, movie_id, score, review) VALUES (?, ?, ?, ?)', [session[:user_id], id, params['score'], params['note']])
       end
     else
-        db.execute('INSERT INTO user_watched (user_id, movie_id, score, review) VALUES (?, ?, ?, ?)', [session[:user_id], id, params['score'], params['note']])
     end
     p userInfo
     redirect("/")
@@ -125,27 +124,6 @@ class App < Sinatra::Base
     id = params['id']
     db.execute("DELETE FROM movies WHERE id = ?", id)
     db.execute("DELETE FROM movies_genres WHERE movie_id = ?", id)
-    redirect("/")
-  end
-
-  get '/review/:id' do | id |
-    @movieinfo = db.execute('SELECT name, year, imdb_rating, runtime, GROUP_CONCAT(DISTINCT genre_name) as genres, poster, movies.id FROM movies
-                            INNER JOIN movies_genres 
-                              ON movies.id = movies_genres.movie_id
-                            INNER JOIN genres
-                              ON movies_genres.genre_id = genres.genre_id
-                            WHERE movies.id = ? 
-                          GROUP BY movies.id ', id).first
-    erb(:"review")
-  end
-
-  post '/review/:id' do | id |
-    updateParams = [params['score'], params['note'], id]
-    db.execute('UPDATE  movies SET 
-                  watched = 1,
-                  score = ?, 
-                  note = ? 
-                WHERE id = ?', updateParams)
     redirect("/")
   end
 end
